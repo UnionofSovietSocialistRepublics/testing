cd786bdb06169411e1a85f2f1a5ccf32803eb511
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-10-24 21:21:40 +0700

    test

--- src/template/ModTemplate.java
+++ src/template/ModTemplate.java
@@ -4,6 +4,7 @@ import mindustry.mod.*;
 import template.gen.*;
 
 public class ModTemplate extends Mod{
+    //FIVE NIGHT AT DIDDY RAAAAAH
     @Override
     public void loadContent(){
         EntityRegistry.register();
